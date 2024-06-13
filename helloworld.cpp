c6ac86fba95314e5dbd9c09412828428ffbf8bbc
Tingyu Ruan <tingyuruanrty@gmail.com>
2024-06-13 11:46:46 -0700

    nothing

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,65 +5,47 @@
 
 using namespace std;
 
-bool match_or_not(char character1, char character2) 
-{
+bool match_or_not(char character1, char character2) {
     if (character1 == '(' && character2 == ')') 
-    {
-        return true;
-    }
+    {return true;}
+
     else if (character1 == '{' && character2 == '}') 
-    {
-        return true;
-    }
+    {return true;}
+
     else if (character1 == '[' && character2 == ']') 
-    {
-        return true;
-    }
+    {return true;}
     return false;
 }
 
 bool are_parentheses_balanced(const string& expression) 
 {
     stack<char> my_stack;
-    for (char c : expression) 
-    {
+    for (char c : expression) {
         if (c == '(' || c == '{' || c == '[') 
-        {
-            my_stack.push(c);
-        } 
+        {my_stack.push(c);} 
         
-        else if (c == ')' || c == '}' || c == ']') 
-        {
+        else if (c == ')' || c == '}' || c == ']') {
             if (my_stack.empty() || !match_or_not(my_stack.top(), c)) 
-            {
-                return false;
-            } 
+            {return false;}
+
             else 
-            {
-                my_stack.pop();
-            }
+            {my_stack.pop();}
         }
     }
     return my_stack.empty();
 }
 
-
-
 int main() 
 {
     string expression;
-    
     cout << "Enter an expression: ";
     cin >> expression;
 
-    if (are_parentheses_balanced(expression)) 
-    {
+    if (are_parentheses_balanced(expression)) {
         cout << "The parentheses are balanced." << endl;
     } 
-    else 
-    {
+    else {
         cout << "The parentheses are not balanced." << endl;
     }
-
     return 0;
 }
