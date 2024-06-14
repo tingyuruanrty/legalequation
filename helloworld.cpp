68ab18fe57ae04999be2eaa0e9ae4686ea12978b
Tingyu Ruan <tingyuruanrty@gmail.com>
2024-06-14 12:16:45 -0700

    finished

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,51 +3,43 @@
 #include <string>
 using namespace std;
 
-stack<char> put_brackets_in_stack(const string& input){
-    stack<char> brackets;
-    for(const char& holder:input)
-    {
-        if(holder=='('||holder=='['||holder =='{')
-        {brackets.push(holder);}
-    }
-    return brackets;
+void put_brackets_in_stack(char holder,stack<char>& brackets){
+    if(holder=='('||holder=='['||holder =='{')
+    {brackets.push(holder);}
 }
 
-bool not_match_or_empty (char a, stack<char>b){
-    if(a==')'&&(b.empty()||b.top()!='(')){
-        return true;
-    }else if(a == ']' && (b.empty()||b.top()!='[')){
-        return true;
-    }else if(a == '}' && (b.empty()||b.top()!='{')){
-        return true;
-    }else{
-        return false;
-    }
+bool not_match_or_empty (char a, stack<char>& b){
+    if(a==')'&&(b.empty()||b.top()!='('))
+    {return true;}
+    else if(a == ']' && (b.empty()||b.top()!='['))
+    {return true;}
+    else if(a == '}' && (b.empty()||b.top()!='{'))
+    {return true;}
+    else{return false;}
 }
-
 
 bool legal(const string& input)
 {
-    stack<char> brackets = put_brackets_in_stack(input);
-
+    stack<char> brackets;
     for(const char& holder:input)
     {
-        if (not_match_or_empty(holder,brackets))
-        {return false;}
-        else 
-        {if (!brackets.empty())brackets.pop();}
+        put_brackets_in_stack(holder,brackets);
+        if(holder==')'||holder==']'||holder=='}')
+        {
+            if (not_match_or_empty(holder,brackets))
+            {return false;}
+            else 
+            {brackets.pop();}
+        }
     }
-
     return brackets.empty();
 }
 
 int main(){
     while(true){
     cout<<"enter equition"<<endl;
-
     string input;
     getline(cin,input);
-
     if(legal(input)){cout<<"legal"<<endl;}
     else{cout<<"illegal"<<endl;}}
 }
